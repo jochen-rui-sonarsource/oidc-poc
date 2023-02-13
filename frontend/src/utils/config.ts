339735816e77ca10524cdc9714f79473e4f3ff6d
Jochen Rui <jochen.rui@sonarsource.com>
2023-02-13 16:16:53 +0100

    add metadata to oidc config

--- frontend/src/utils/config.ts
+++ frontend/src/utils/config.ts
@@ -5,4 +5,10 @@ export const OIDC_CONFIG = {
   response_type: import.meta.env.VITE_RESPONSE_TYPE || "", //(string: can be "code" or "token id_token"/"id_token token")
   scope: import.meta.env.VITE_SCOPE || "", //(string): defines what's included in the ID token. Either "openid profile", "openid email" or "openid profile email". Can also include more scopes
   redirect_uri: import.meta.env.VITE_REDIRECT_URI || "", //(string): The URL to redirect the user to after authentication
+  metadata: {
+    authorization_endpoint: import.meta.env.VITE_AUTHORIZATION_ENDPOINT || "", //(string):
+    issuer: import.meta.env.VITE_AUTH_URI || "", //(string): The URL of the OIDC
+    userinfo_endpoint: import.meta.env.VITE_USERINFO_ENDPOINT,
+    end_session_endpoint: import.meta.env.VITE_END_SESSION_ENDPOINT || "", //(string):
+  },
 };
